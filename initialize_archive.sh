d9a6d179a3992dd9ef7d220a7a938558963a7f65
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2022-05-27 15:13:42 +0200

    Store keys outside of Borg archive

--- initialize_archive.sh
+++ initialize_archive.sh
@@ -9,10 +9,11 @@ set -euo pipefail
 RCLOUD_CONFIG="/var/backup/config"
 BORG_ARCHIVE="/var/backup/snapshots"
 BORG_PATH_TO_BACKUP="/mnt/source"
+export BORG_KEYS_DIR="/var/backup/keys"
 
 # Configure Borg
 mkdir -p $BORG_ARCHIVE
-borg init --encryption=repokey $BORG_ARCHIVE
+borg init --encryption=keyfile $BORG_ARCHIVE
 
 # Configure rclone
 rclone config --config $RCLOUD_CONFIG
@@ -20,5 +21,5 @@ rclone config --config $RCLOUD_CONFIG
 echo
 echo "Configuration completed!"
 echo "Run your first snapshot with the following command:"
-echo "    $ docker run --rm -ti --volume $PWD/to_backup:/mnt/source:z --volume $PWD/testing:/var/backup:z --env-file env.list photography-backup:latest create_snapshot"
+echo "    $ docker run --rm -ti --env-file env.list --volume <PATH_TO_BACKUP>:/mnt/source --volume <BORG_ARCHIVE_PATH>:/var/backup photography-backup:latest create_snapshot"
 echo
